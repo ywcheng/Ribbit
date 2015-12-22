b6fd939d07e7edd3fb453cfe3e10a9eef23667f1
YW <yuahwen.cheng@gmail.com>
2015-12-22 16:59:04 +0800

    FriendRelation added

---
+++ app/src/main/java/com/hqetpe/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.hqetpe.ribbit;
+
+/**
+ * Created by YW on 12/21/15.
+ */
+public final class ParseConstants {
+    //Class name
+
+    //Field names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation" ;
+}
